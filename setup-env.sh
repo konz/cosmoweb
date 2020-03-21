d6edc796621c23bae3101ffc62f316738036c6d0
Konrad Hosemann <konrad.hosemann@solarisbank.de>
2020-03-21 18:26:40 +0100

    fix certificate issue by switching to new endpoint, see https://aws.amazon.com/blogs/iot/aws-iot-core-ats-endpoints/

--- setup-env.sh
+++ setup-env.sh
@@ -3,5 +3,5 @@ export IDENTITY_POOL_ID=$(aws cloudformation describe-stacks --stack-name cosmow
 # get the generated S3 bucket
 export BUCKET=$(aws cloudformation describe-stacks --stack-name cosmoweb --query "Stacks[0].Outputs[?OutputKey=='bucketName'].OutputValue" --output text)
 
-export IOT_ENDPOINT=$(aws iot describe-endpoint --output text)
+export IOT_ENDPOINT=$(aws iot describe-endpoint --endpoint-type iot:Data-ATS --output text)
 export AWS_REGION=$(aws configure get region)
