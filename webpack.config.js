2f8ec8ab4afa80d79dccfc5586d05e0875bad774
Konrad Hosemann <konrad.hosemann@solarisbank.de>
2020-03-21 18:27:39 +0100

    some package updates, remove minify plugin for now - it's not compatoíble with latest webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 const webpack = require('webpack');
 const path = require('path');
 
@@ -12,14 +11,13 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([{
       from: 'static',
-      ignore: '.DS_Store'
+      ignore: ['.DS_Store']
     }]),
     new webpack.DefinePlugin({
       REGION: JSON.stringify(process.env.AWS_REGION),
       IDENTITY_POOL_ID: JSON.stringify(process.env.IDENTITY_POOL_ID),
       IOT_ENDPOINT: JSON.stringify(process.env.IOT_ENDPOINT)
     }),
-    new MinifyPlugin()
   ],
   node: {
     fs: 'empty',
